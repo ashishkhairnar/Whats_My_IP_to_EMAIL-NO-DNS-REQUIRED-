1b187839433c9c93e1949cb5251ddfa8680a22be
Ashish Khairnar <ashish20171990@gmail.com>
2018-11-17 22:37:12 -0500

    Update whats_my_script.py

--- whats_my_script.py
+++ whats_my_script.py
@@ -6,23 +6,45 @@ import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
-get_request = requests.get("http://www.ipvoid.com")
-soup = bs4(get_request.content,'lxml')
-ip = soup.find('input', {'name':''})['value']
+fromaddr = "<YOUR GMAIL EMAIL ADDRESS>" 
+toaddr = "<DESIRED EMAIL ADDRESS>"
+
+msg = MIMEMultipart()
 
 # Turn on "Less Secure App for Google accounts. Refer - https://support.google.com/accounts/answer/6010255?hl=en"
 
-fromaddr = "<YOUR GMAIL EMAIL ADDRESS>" 
-toaddr = "<DESIRED EMAIL ADDRESS>"
-msg = MIMEMultipart()
 msg['From'] = fromaddr
 msg['To'] = toaddr
-msg['Subject'] = "Current IP is " + ip
 
-server = smtplib.SMTP('smtp.gmail.com', 587) #change appropriate if using other than GMail
-server.ehlo()
-server.starttls()
-server.ehlo()
-server.login(fromaddr, '<PASSWORD>')
-text = msg.as_string()
-server.sendmail(fromaddr, toaddr, text)
+#Make a text file in the same directory for storing current value and avoid repititive emails.
+
+PATH_INPUT = "/<Whatever directory>/current_ip.txt"
+
+get_request = requests.get("http://www.ipvoid.com")
+soup = bs4(get_request.content,'lxml')
+current_ip = soup.find('input', {'name':''})['value']
+print "Current IP is " + current_ip
+
+with open(PATH_INPUT, "r") as f:
+	existing_IP = f.readlines()
+	for line in existing_IP:
+		existing_IP = str(line).strip('\n')
+	f.close()
+
+if existing_IP == current_ip:
+	print "no email sent!"
+
+else:
+	msg['Subject'] = "Current IP is " + current_ip
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.ehlo()
+	server.starttls()
+	server.ehlo()
+	server.login(fromaddr, '<PASSWORD>')  #logins
+	text = msg.as_string()
+	
+	server.sendmail(fromaddr, toaddr, text) #sends email
+	
+	with open(PATH_INPUT, "w") as f:
+		f.write(str(current_ip + "\n"))
+		f.close()
